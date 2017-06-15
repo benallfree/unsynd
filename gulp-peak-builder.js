14f40f50fd67c44c3b7a27564e77077d0cfb8675
Ben Allfree <ben@benallfree.com>
2017-06-15 12:49:33 -0700

    Peak building and previews

--- gulp-peak-builder.js
+++ gulp-peak-builder.js
@@ -14,21 +14,26 @@ module.exports = function() {
   return through.obj(function(file, enc, cb) {
     var self = this;
 
-    var tmpobj = tmp.fileSync({postfix: '.json'});
+    var tmpobj = tmp.fileSync({postfix: '.json', keep: true});
     var cmd = `audiowaveform -i "${file.path}" -b 8 -o "${tmpobj.name}"`
     console.log(cmd)
     exec(cmd, function(error, stdout, stderr) {
       let samples = require(tmpobj.name).data;
-      samples = samples.filter( (v)=> { return v>=0; })
+      samples = samples.filter( (v,i)=> { return i%2==1; })
       var peaks = [], size = samples.length/300;
-
-      while (samples.length > 0)
+      for(var i=0; i < samples.length; i+=2)
       {
-        var t = samples.splice(0, size).reduce( (a,v)=>{ return a+v;} );
-        peaks.push((t/size/127.0).toFixed(4)*1)
+        peaks.push(((Math.abs(samples[i])+samples[i+1])/256));
+      }
+      var final = [], size = peaks.length/300;
+      console.log("size is", size);
+      while(peaks.length>0)
+      {
+        var t = peaks.splice(0,size).reduce((a,v) => { return a+v; });
+        final.push((t/size).toFixed(4)*1);
       }
       
-      file.contents = new Buffer(JSON.stringify(peaks));
+      file.contents = new Buffer(JSON.stringify(final));
       file.path = file.path.replace(/\.[^/.]+$/, ".json");
       self.push(file);
       cb();
